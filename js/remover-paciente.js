3c448191fa5ef6d345fb61525aabf6519f0d6a61
Davi Roberto <daviroberto11@hotmail.com>
2017-05-27 17:59:08 -0300

    Removendo paciente da tabela - COM ANIMAÇÃO BÁSICA

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -3,6 +3,11 @@ var tabela = document.querySelector("table");
 
 // Adiciono o evento 'dblclick' pra quando a variável tabela ouvir o evento 'dblclick' execulta a função anônima
 tabela.addEventListener("dblclick", function(event){
-	event.target.parentNode.remove();
-//	evento.foco.selecionaOPaiDoEventoE.Remove(); 
+	event.target.parentNode.classList.add("fadeOut");
+
+	setTimeout(function() {
+		event.target.parentNode.remove();
+	//	evento.foco.selecionaOPaiDoEventoE.Remove(); 
+	}, 700);
+
 });
